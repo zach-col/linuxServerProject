4ee9a74d8262caff13c983b37cc09a36e174b62b
zach <theperfectpicture100@gmail.com>
2018-03-14 18:15:03 -0500

    Update lotsOfCatalogs.py

--- lotsOfCatalogs.py
+++ lotsOfCatalogs.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker
 
 from database_setup import Catalog, Base, CatalogItem, User
 
-$ engine = create_engine('postgresql://catalog:PASSWORD_GOES_HERE@localhost/catalog')
+engine = create_engine('postgresql://catalog:PASSWORD_GOES_HERE@localhost/catalog')
 # Bind the engine to the metadata of the Base class so that the
 # declaratives can be accessed through a DBSession instance
 Base.metadata.bind = engine
